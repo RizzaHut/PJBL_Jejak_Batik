97b779ff5c3580b3d2138305175659661de96cde
Rizza Cetta Ugama <ugamarizzacetta@gmail.com>
2026-05-18 12:43:49 +0700

    feat: project setup

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+import 'app/modules/home/controllers/nav_controller.dart';
+import 'app/routes/app_pages.dart';
+import 'app/routes/app_routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Get.put(NavController(), permanent: true);
+  runApp(
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(body: Center(child: CircularProgressIndicator())),
+          );
+        }
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(fontFamily: 'Poppins'),
+          initialRoute: snapshot.data != null ? AppPages.INITIAL : Routes.login,
+          getPages: AppPages.pages,
+        );
+      },
+    ),
+  );
+}
